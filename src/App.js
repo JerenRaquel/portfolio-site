2af65c52ea905a61998cbd815cf1f65ce677f0f4
JerenRaquel <jeraquel101@gmail.com>
2021-08-06 20:34:58 -0700

    added a nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import { makeStyles } from '@material-ui/styles'
 
 function App() {
+  const classes = styles();
+
   return (
-    <div>
-      Hello There!
-    </div>
+    <>
+      <div className={classes.container}>
+        <div className={classes.navBar}>
+          <div className={classes.items}>Home</div>
+          <div className={classes.items}>Projects</div>
+          <div className={classes.items}>About</div>
+          <div className={classes.items}>Contact</div>
+        </div>
+      </div>
+    </>
   );
 }
+
+const styles = makeStyles({
+  container: {
+    display: "flex",
+    justifyContent: "center"
+  },
+  navBar: {
+    display: "flex",
+    maxWidth: 600,
+    marginTop: "2em"
+  },
+  items: {
+    margin: "0 1em"
+  }
+})
 
 export default App;
